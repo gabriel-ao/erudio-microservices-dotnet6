710201b1ef5bb0567970fa91d2f6e3103cf8f3aa
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-04-04 00:46:05 -0300

    0809 Implementando o Controller do Microsserviço de Produtos

---
+++ S08-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,54 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region
+
+// conexão ao banco de dados
+ConfigurationManager configuration = builder.Configuration;
+
+var connection = configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+        UseMySql(connection, 
+            new MySqlServerVersion(
+                new Version(8, 0, 5))));  // confirmar qual versão do MYSQL
+
+// map para varrer dados
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// injeção de dependencias
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+#endregion
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+
+
+app.MapControllers();
+
+app.Run();
