b8c39a2a7f873d8d106d99228aaf87c3d1616aed
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-04-07 18:36:27 -0300

    0904 ENtendendo e implementando a classe HttpClientExtensions

--- S09-ConsumingProductMicroserviceonWebMicroservice/GeekShopping.Web/Services/IServices/IProductService.cs
+++ S09-ConsumingProductMicroserviceonWebMicroservice/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -8,6 +8,8 @@ namespace GeekShopping.Web.Services.IServices
 
         Task<ProductModel> FindProductById(long id);
         Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
 
 
 
