27806a5e8824a63e3fb6500bffda3494a2eebe4b
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-04-15 18:43:05 -0300

    1119 Adicionando Autenticação ao Swagger (Open API)

--- S11-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping.ProductAPI/Program.cs
+++ S11-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping.ProductAPI/Program.cs
@@ -39,12 +39,41 @@ builder.Services.AddAuthorization(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping" });
-});
+
 
 #region CONFIGURAÇÂO DO CURSO DE ASPNET
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string> ()
+        }
+    });
+});
 
 // conexão ao banco de dados
 ConfigurationManager configuration = builder.Configuration;
