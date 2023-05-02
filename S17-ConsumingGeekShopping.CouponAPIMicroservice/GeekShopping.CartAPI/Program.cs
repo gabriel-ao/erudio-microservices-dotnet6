e03cdbcdad875f320993440fdd0c13b2529bf4c6
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-05-02 00:34:28 -0300

    174. 1701 Apresentação da Seção - Consumindo o Microsserviço de Cupom de Desconto

---
+++ S17-ConsumingGeekShopping.CouponAPIMicroservice/GeekShopping.CartAPI/Program.cs
@@ -0,0 +1,131 @@
+using AutoMapper;
+using GeekShopping.CartAPI.Config;
+using GeekShopping.CartAPI.Model.Context;
+using GeekShopping.CartAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+#region teste Tarcio Rodrigo Almeida dos custo
+//builder.Services.AddControllers();
+
+builder.Services.AddControllers(options => 
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+
+#endregion
+
+
+
+#region CONFIGURAÇÂO DO CURSO DE ASPNET
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.Authority = "https://localhost:4435";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+    };
+});
+//TODO - PASSAR ACIMA PARA O LAUNCH PRA TREINO
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+#endregion 
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+#region CONFIGURAÇÂO DO CURSO DE ASPNET
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.CartAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string> ()
+        }
+    });
+});
+
+
+// conexão ao banco de dados
+ConfigurationManager configuration = builder.Configuration;
+
+var connection = configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+        UseMySql(connection,
+            new MySqlServerVersion(
+                new Version(8, 0, 5))));  // confirmar qual versão do MYSQL
+
+// map para varrer dados - olhar na pasta dos VOs
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// injeção de dependencias
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+
+#endregion
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+#region CONFIGURAÇÂO DO CURSO DE ASPNET
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+#endregion
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
