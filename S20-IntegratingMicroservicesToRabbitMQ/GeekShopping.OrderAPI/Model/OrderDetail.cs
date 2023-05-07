519803b7c6f4acd4a1f433d9b20cf9afaf1e99b0
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-05-07 12:26:58 -0300

    200. 2103 Configurando o Startup e DbContext do GeekShopping.OrderAPI Microsservice

---
+++ S20-IntegratingMicroservicesToRabbitMQ/GeekShopping.OrderAPI/Model/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using GeekShopping.OrderAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.OrderAPI.Model
+{
+    [Table("order_details")]
+    public class OrderDetail : BaseEntity
+    {
+        public long CartHeaderId { get; set; }
+
+
+        [ForeignKey("CartHeaderId")]
+        public virtual CartHeader CartHeader { get; set; }
+        public long ProductId { get; set; }
+
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+    }
+}
