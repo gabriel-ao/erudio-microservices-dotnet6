e514c8ab3dcf9462acf8104cb1028a0afe2f164c
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-05-07 01:25:00 -0300

    189. 1807 Comunicação Síncrona X Assíncrona

--- S18-IntegratingCheckout/GeekShopping.CartAPI/Controllers/CartController.cs
+++ S18-IntegratingCheckout/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -65,6 +66,21 @@ namespace GeekShopping.CartAPI.Controllers
 
             if (!status) return NotFound();
             return Ok(status);
+        }
+
+        [HttpPost("checkout")]
+        public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
+        {
+            var cart = await _repository.FindCartByUserId(vo.UserId);
+
+            if (cart == null) return NotFound();
+
+            vo.CartDetails = cart.CartDetails;
+            vo.DateTime = DateTime.Now;
+
+            // TODO - RabbitMQ logic comes here!!!
+
+            return Ok(vo);
         }
     }
 }
