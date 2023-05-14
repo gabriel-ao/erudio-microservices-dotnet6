28078658751c0582f3e18dbee1f61d3fe191558c
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-05-13 23:54:15 -0300

    219. 2301 Apresentação da seção - Criação do Microsserviço de Pagamentos

---
+++ S22-SyncCommunicationBetweenMicroservices/GeekShopping.Web/Program.cs
@@ -0,0 +1,71 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+#region CONFIGURAÇÂO DO CURSO DE ASPNET
+
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
+
+
+builder.Services.AddHttpClient<ICartService, CartService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"]));
+
+
+builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
+
+#endregion
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#region CONFIGURAÇÂO DO CURSO DE ASPNET
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+}).AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+.AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.ClientId = "geek_shopping";
+    options.ClientSecret = "my_super_secret";
+    options.ResponseType = "code";
+    options.ClaimActions.MapJsonKey("role", "role", "role");
+    options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+    options.TokenValidationParameters.NameClaimType = "name";
+    options.TokenValidationParameters.RoleClaimType = "role";
+    options.Scope.Add("geek_shopping");
+    options.SaveTokens = true;
+});
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+#region CONFIGURAÇÂO DO CURSO DE ASPNET
+app.UseAuthentication();
+#endregion
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
