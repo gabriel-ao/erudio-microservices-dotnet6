238475ffdef9204f3628da8fa6ffcd8e8363c199
DESKTOP-98QJDF1\gabri <gabrielao.developer@gmail.com>
2023-05-17 23:28:14 -0300

    232. 2402 Refatorando o RabbitMQMessageSender para Enviar Excccchanges do Tipo Fanout

---
+++ S23-CreatingPaymentAPIMicroservice/GeekShopping.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,10 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+
+namespace GeekShopping.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponVO> GetCoupon(string couponCode);
+
+    }
+}
